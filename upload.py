93ff4a356ed2033278494b74697211c3b2269874
Jamarr Lampart <jamarr.lampart@snowflake.com>
2023-04-18 14:28:24 -0700

    Create upload.py

---
+++ upload.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+st.title("Document Uploader")
+
+uploaded_file = st.file_uploader("Choose a file", type=["pdf", "docx", "txt"])
+
+if uploaded_file is not None:
+    file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
+    st.write(file_details)
+
+    with open(uploaded_file.name, "wb") as f:
+        f.write(uploaded_file.getbuffer())
+    st.success("File successfully uploaded!")
