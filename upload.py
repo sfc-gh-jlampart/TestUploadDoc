7146a1dcdc659aef06daf490b3ca6fcea12268aa
Jamarr Lampart <jamarr.lampart@snowflake.com>
2023-04-19 08:33:41 -0700

    Update upload.py

--- upload.py
+++ upload.py
@@ -4,10 +4,10 @@ st.title("Document Uploader")
 
 uploaded_file = st.file_uploader("Choose a file", type=["pdf", "docx", "txt"])
 
-if uploaded_file is not None:
-    file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
-    st.write(file_details)
+# if uploaded_file is not None:
+#     file_details = {"FileName": uploaded_file.name, "FileType": uploaded_file.type}
+#     st.write(file_details)
 
-    with open(uploaded_file.name, "wb") as f:
-        f.write(uploaded_file.getbuffer())
-    st.success("File successfully uploaded!")
+#     with open(uploaded_file.name, "wb") as f:
+#         f.write(uploaded_file.getbuffer())
+#     st.success("File successfully uploaded!")
